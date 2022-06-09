0ab92c3c4b83f3e6154e9a3f44051ef0ebeb2672
Newbie012 <co.eliya2@gmail.com>
2022-06-09 12:18:50 +0300

    pre-transform design-system package

--- packages/my-app/vite.config.ts
+++ packages/my-app/vite.config.ts
@@ -1,26 +1,30 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// @ts-ignore
+import { babelTransform } from "../vite-utils";
+
+const emotionPlugin = [
+  "@emotion",
+  {
+    importMap: {
+      theming: {
+        styled: { canonicalImport: ["@emotion/styled", "default"] },
+        css: { canonicalImport: ["@emotion/react", "css"] },
+        Global: { canonicalImport: ["@emotion/react", "Global"] },
+      },
+    },
+  },
+];
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    babelTransform([/design-system/], { plugins: [emotionPlugin] }),
     react({
       babel: {
-        plugins: [
-          [
-            "@emotion",
-            {
-              importMap: {
-                theming: {
-                  styled: { canonicalImport: ["@emotion/styled", "default"] },
-                  css: { canonicalImport: ["@emotion/react", "css"] },
-                  Global: { canonicalImport: ["@emotion/react", "Global"] }
-                }
-              }
-            }
-          ]
-        ]
-      }
-    })
-  ]
+        plugins: [emotionPlugin],
+      },
+    }),
+  ],
 });
---
+++ packages/vite-utils/src/index.ts
@@ -0,0 +1,1 @@
+export { default as babelTransform } from "./vite-plugin-babel-transform"
